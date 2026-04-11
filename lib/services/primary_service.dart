a10c50f3f557c49fb366ae2c067a44fb67b6a540
Isaac Moore <ihmoore@wpi.edu>
2026-04-10 21:11:46 -0400

    small change to remove useless imports that were useful earlier

--- lib/services/primary_service.dart
+++ lib/services/primary_service.dart
@@ -1,14 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:a4g/model.dart';
-import 'package:flutter/foundation.dart';
-
 class FirstAidService {
-  static const String _prefLargeText = 'pref_large_text';
-  static const String _prefShowImages = 'pref_show_images';
-  static const String _prefDarkMode = 'pref_dark_mode';
-
   // ─── Load JSON from assets ───────────────────────────────────────────────
   Future<List<AilmentCategory>> loadCategories() async {
     final String jsonString =
